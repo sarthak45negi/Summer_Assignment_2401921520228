662a524b2ccfea1baad06e11dfbe79cb8a4953e6
Sarthak Negi <sarthaknegi225@gmail.com>
2026-06-14 21:23:30 +0530

    Week 2 OOPS Assignment

---
+++ Week_2/OOPS/live/test.java
@@ -0,0 +1,27 @@
+package live;
+
+import music.playable;
+import music.string.Veena;
+import music.wind.saxophone;
+
+class Test {
+    public static void main(String[] args){
+        // a. Instance of Veena
+        Veena v = new Veena();
+        v.play();
+
+        // b. Instance of Saxophone
+        saxophone s = new saxophone();
+        s.play();
+
+        // c. Using Playable reference
+        playable p;
+
+        p = new Veena();
+        p.play();
+
+        p = new saxophone();
+        p.play();
+    }
+}
+
