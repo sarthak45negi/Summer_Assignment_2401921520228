e867ad4f6b81a0ef598e7ef02ed8b729625f8731
Sarthak Negi <sarthaknegi225@gmail.com>
2026-06-24 21:18:24 +0530

    Assignment

---
+++ Week_4/Day_1/Q1.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == nullptr)return 0;
+        queue<TreeNode*>q;
+        q.push(root);
+        int cnt = 0;
+        while(!q.empty()){
+            int n = q.size();
+            for(int i = 0 ; i < n ; i++){
+                TreeNode* node = q.front();
+                q.pop();
+                if(node -> left != nullptr) q.push(node->left);
+                if(node -> right != nullptr) q.push(node->right);
+            }
+            cnt++;
+        }
+        return cnt;
+    }
+};
